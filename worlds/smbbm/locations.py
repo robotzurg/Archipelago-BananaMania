af176e8ed67c98f82531844b10124f56248c6ca7
Jeff Turpin <robotzurg@gmail.com>
2026-03-07 23:14:06 -0700

    Add locations, regions, rules, and world stuff

--- worlds/smbbm/locations.py
+++ worlds/smbbm/locations.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from BaseClasses import ItemClassification, Location
+from BaseClasses import Location
 
 from . import items
 
@@ -460,6 +460,40 @@ LOCATION_NAME_TO_ID = {
     "[W10-10] Banana Collector 25": 992,
     "[W10-10] Time Limit 25": 993,
 
+    # Shop Locations
+    "Banana Shop Item 1": 1000,
+    "Banana Shop Item 2": 1001,
+    "Banana Shop Item 3": 1002,
+    "Banana Shop Item 4": 1003,
+    "Banana Shop Item 5": 1004,
+    "Banana Shop Item 6": 1005,
+    "Banana Shop Item 7": 1006,
+    "Banana Shop Item 8": 1007,
+    "Banana Shop Item 9": 1008,
+    "Banana Shop Item 10": 1009,
+    "Banana Shop Item 11": 1010,
+    "Banana Shop Item 12": 1011,
+    "Banana Shop Item 13": 1012,
+    "Banana Shop Item 14": 1013,
+    "Banana Shop Item 15": 1014,
+    "Banana Shop Item 16": 1015,
+    "Banana Shop Item 17": 1016,
+    "Banana Shop Item 18": 1017,
+    "Banana Shop Item 19": 1018,
+    "Banana Shop Item 20": 1019,
+    "Banana Shop Item 21": 1020,
+    "Banana Shop Item 22": 1021,
+    "Banana Shop Item 23": 1022,
+    "Banana Shop Item 24": 1023,
+    "Banana Shop Item 25": 1024,
+    "Banana Shop Item 26": 1025,
+    "Banana Shop Item 27": 1026,
+    "Banana Shop Item 28": 1027,
+    "Banana Shop Item 29": 1028,
+    "Banana Shop Item 30": 1029,
+    "Banana Shop Item 31": 1030,
+    "Banana Shop Item 32": 1031,
+
 }
 
 
@@ -485,91 +519,26 @@ def create_all_locations(world: BananaManiaWorld) -> None:
 
 
 def create_regular_locations(world: BananaManiaWorld) -> None:
-    # Finally, we need to put the Locations ("checks") into their regions.
-    # Once again, before we do anything, we can grab our regions we created by using world.get_region()
-    overworld = world.get_region("Overworld")
-    top_left_room = world.get_region("Top Left Room")
-    bottom_right_room = world.get_region("Bottom Right Room")
-    right_room = world.get_region("Right Room")
+    # Group stage locations by world number (W1 through W10).
+    for world_num in range(1, 11):
+        prefix = f"[W{world_num}-"
+        region = world.get_region(f"World {world_num}")
+        stage_locations = get_location_names_with_ids(
+            [name for name in LOCATION_NAME_TO_ID if name.startswith(prefix)]
+        )
+        region.add_locations(stage_locations, BananaManiaLocation)
 
-    # One way to create locations is by just creating them directly via their constructor.
-    bottom_left_chest = BananaManiaLocation(
-        world.player, "Bottom Left Chest", world.location_name_to_id["Bottom Left Chest"], overworld
+    # Banana Shop locations.
+    banana_shop = world.get_region("Banana Shop")
+    shop_locations = get_location_names_with_ids(
+        [name for name in LOCATION_NAME_TO_ID if name.startswith("Banana Shop Item")]
     )
-
-    # You can then add them to the region.
-    overworld.locations.append(bottom_left_chest)
-
-    # A simpler way to do this is by using the region.add_locations helper.
-    # For this, you need to have a dict of location names to their IDs (i.e. a subset of location_name_to_id)
-    # Aha! So that's why we made that "get_location_names_with_ids" helper method earlier.
-    # You also need to pass your overridden Location class.
-    bottom_right_room_locations = get_location_names_with_ids(
-        ["Bottom Right Room Left Chest", "Bottom Right Room Right Chest"]
-    )
-    bottom_right_room.add_locations(bottom_right_room_locations, BananaManiaLocation)
-
-    top_left_room_locations = get_location_names_with_ids(["Top Left Room Chest"])
-    top_left_room.add_locations(top_left_room_locations, BananaManiaLocation)
-
-    right_room_locations = get_location_names_with_ids(["Right Room Enemy Drop"])
-    right_room.add_locations(right_room_locations, BananaManiaLocation)
-
-    # Locations may be in different regions depending on the player's options.
-    # In our case, the hammer option puts the Top Middle Chest into its own room called Top Middle Room.
-    top_middle_room_locations = get_location_names_with_ids(["Top Middle Chest"])
-    if world.options.hammer:
-        top_middle_room = world.get_region("Top Middle Room")
-        top_middle_room.add_locations(top_middle_room_locations, BananaManiaLocation)
-    else:
-        overworld.add_locations(top_middle_room_locations, BananaManiaLocation)
-
-    # Locations may exist only if the player enables certain options.
-    # In our case, the extra_starting_chest option adds the Bottom Left Extra Chest location.
-    if world.options.extra_starting_chest:
-        # Once again, it is important to stress that even though the Bottom Left Extra Chest location doesn't always
-        # exist, it must still always be present in the world's location_name_to_id.
-        # Whether the location actually exists in the seed is purely determined by whether we create and add it here.
-        bottom_left_extra_chest = get_location_names_with_ids(["Bottom Left Extra Chest"])
-        overworld.add_locations(bottom_left_extra_chest, BananaManiaLocation)
+    banana_shop.add_locations(shop_locations, BananaManiaLocation)
 
 
 def create_events(world: BananaManiaWorld) -> None:
-    # Sometimes, the player may perform in-game actions that allow them to progress which are not related to Items.
-    # In our case, the player must press a button in the top left room to open the final boss door.
-    # AP has something for this purpose: "Event locations" and "Event items".
-    # An event location is no different than a regular location, except it has the address "None".
-    # It is treated during generation like any other location, but then it is discarded.
-    # This location cannot be "sent" and its item cannot be "received", but the item can be used in logic rules.
-    # Since we are creating more locations and adding them to regions, we need to grab those regions again first.
-    top_left_room = world.get_region("Top Left Room")
-    final_boss_room = world.get_region("Final Boss Room")
-
-    # One way to create an event is simply to use one of the normal methods of creating a location.
-    button_in_top_left_room = BananaManiaLocation(world.player, "Top Left Room Button", None, top_left_room)
-    top_left_room.locations.append(button_in_top_left_room)
-
-    # We then need to put an event item onto the location.
-    # An event item is an item whose code is "None" (same as the event location's address),
-    # and whose classification is "progression". Item creation will be discussed more in items.py.
-    # Note: Usually, items are created in world.create_items(), which for us happens in items.py.
-    # However, when the location of an item is known ahead of time (as is the case with an event location/item pair),
-    # it is common practice to create the item when creating the location.
-    # Since locations also have to be finalized after world.create_regions(), which runs before world.create_items(),
-    # we'll create both the event location and the event item in our locations.py code.
-    button_item = items.BananaManiaItem("Top Left Room Button Pressed", ItemClassification.progression, None, world.player)
-    button_in_top_left_room.place_locked_item(button_item)
-
-    # A way simpler way to do create an event location/item pair is by using the region.create_event helper.
-    # Luckily, we have another event we want to create: The Victory event.
-    # We will use this event to track whether the player can win the game.
-    # The Victory event is a completely optional abstraction - This will be discussed more in set_rules().
-    final_boss_room.add_event(
-        "Final Boss Defeated", "Victory", location_type=BananaManiaLocation, item_type=items.BananaManiaItem
+    # Place the Victory event on World 10, representing completion of the final world.
+    world_10 = world.get_region("World 10")
+    world_10.add_event(
+        "Cleared World 10", "Victory", location_type=BananaManiaLocation, item_type=items.BananaManiaItem
     )
-
-    # If you create all your regions and locations line-by-line like this,
-    # the length of your create_regions might get out of hand.
-    # Many worlds use more data-driven approaches using dataclasses or NamedTuples.
-    # However, it is worth understanding how the actual creation of regions and locations works,
-    # That way, we're not just mindlessly copy-pasting! :)
--- worlds/smbbm/options.py
+++ worlds/smbbm/options.py
@@ -18,135 +18,70 @@ from Options import Choice, OptionGroup, PerGameCommonOptions, Range, Toggle
 # A toggle is an option that can either be on or off. This will be represented by a checkbox on the website.
 # The default for a toggle is "off".
 # If you want a toggle to be on by default, you can use the "DefaultOnToggle" class instead of the "Toggle" class.
-class HardMode(Toggle):
+class IncludeBananaMissions(Toggle):
     """
-    In hard mode, the basic enemy and the final boss will have more health.
-    The Health Upgrades become progression, as they are now required to beat the final boss.
+    Includes the Banana Collect Missions. These are more difficult.
     """
 
     # The docstring of an option is used as the description on the website and in the template yaml.
 
     # You'll also want to set a display name, which will determine what the option is called on the website.
-    display_name = "Hard Mode"
+    display_name = "Banana Collect Missions"
 
-
-class Hammer(Toggle):
+class IncludeTimeMissions(Toggle):
     """
-    Adds another item to the itempool: The Hammer.
-    The top middle chest will now be locked behind a breakable wall, requiring the Hammer.
+    Includes the Time Limit Missions. These are more difficult.
     """
 
-    display_name = "Hammer"
+    # The docstring of an option is used as the description on the website and in the template yaml.
 
+    # You'll also want to set a display name, which will determine what the option is called on the website.
+    display_name = "Time Limit Missions"
 
-class ExtraStartingChest(Toggle):
+class IncludeExtraGoals(Toggle):
     """
-    Adds an extra chest in the bottom left, making room for an extra Confetti Cannon.
-    """
-
-    display_name = "Extra Starting Chest"
-
-
-class TrapChance(Range):
-    """
-    Percentage chance that any given Confetti Cannon will be replaced by a Math Trap.
+    Includes the extra goals as checks. These are more difficult.
     """
 
-    display_name = "Trap Chance"
+    # The docstring of an option is used as the description on the website and in the template yaml.
 
-    range_start = 0
-    range_end = 100
-    default = 0
+    # You'll also want to set a display name, which will determine what the option is called on the website.
+    display_name = "Extra Goals"
 
-
-class StartWithOneConfettiCannon(Toggle):
+class WorldStructure(Choice):
     """
-    Start with a confetti cannon already in your inventory.
-    Why? Because you deserve it. You get to celebrate yourself without doing any work first.
-    """
-
-    display_name = "Start With One Confetti Cannon"
-
-
-# A Range is a numeric option with a min and max value. This will be represented by a slider on the website.
-class ConfettiExplosiveness(Range):
-    """
-    How much confetti each use of a confetti cannon will fire.
-    """
-
-    display_name = "Confetti Explosiveness"
-
-    range_start = 0
-    range_end = 10
-
-    # Range options must define an explicit default value.
-    default = 3
-
-
-# A Choice is an option with multiple discrete choices. This will be represented by a dropdown on the website.
-class PlayerSprite(Choice):
-    """
-    The sprite that the player will have.
+    How the world items should be structured.
+    progression: Receive "Progressive World Unlock", which unlocks the next world in order.
+    worlds: Receive specific world unlocks, making you do worlds out of order.
     """
 
     display_name = "Player Sprite"
 
-    option_human = 0
-    option_duck = 1
-    option_horse = 2
-    option_cat = 3
+    option_progression = 0
+    option_worlds = 1
 
     # Choice options must define an explicit default value.
-    default = option_human
-
-    # For choices, you can also define aliases.
-    # For example, we could make it so "player_sprite: kitty" resolves to "player_sprite: cat" like this:
-    alias_kitty = option_cat
+    default = option_progression
 
 
 # We must now define a dataclass inheriting from PerGameCommonOptions that we put all our options in.
 # This is in the format "option_name_in_snake_case: OptionClassName".
 @dataclass
-class APQuestOptions(PerGameCommonOptions):
-    hard_mode: HardMode
-    hammer: Hammer
-    extra_starting_chest: ExtraStartingChest
-    start_with_one_confetti_cannon: StartWithOneConfettiCannon
-    trap_chance: TrapChance
-    confetti_explosiveness: ConfettiExplosiveness
-    player_sprite: PlayerSprite
+class BananaManiaOptions(PerGameCommonOptions):
+    banana_missions: IncludeBananaMissions
+    time_missions: IncludeTimeMissions
+    extra_goals: IncludeExtraGoals
+    world_structure: WorldStructure
 
 
 # If we want to group our options by similar type, we can do so as well. This looks nice on the website.
 option_groups = [
     OptionGroup(
-        "Gameplay Options",
-        [HardMode, Hammer, ExtraStartingChest, StartWithOneConfettiCannon, TrapChance],
+        "Mission Options",
+        [IncludeBananaMissions, IncludeTimeMissions, IncludeExtraGoals],
     ),
     OptionGroup(
-        "Aesthetic Options",
-        [ConfettiExplosiveness, PlayerSprite],
+        "Item Options",
+        [WorldStructure],
     ),
 ]
-
-# Finally, we can define some option presets if we want the player to be able to quickly choose a specific "mode".
-option_presets = {
-    "boring": {
-        "hard_mode": False,
-        "hammer": False,
-        "extra_starting_chest": False,
-        "start_with_one_confetti_cannon": False,
-        "trap_chance": 0,
-        "confetti_explosiveness": ConfettiExplosiveness.range_start,
-        "player_sprite": PlayerSprite.option_human,
-    },
-    "the true way to play": {
-        "hard_mode": True,
-        "hammer": True,
-        "extra_starting_chest": True,
-        "start_with_one_confetti_cannon": True,
-        "trap_chance": 50,
-        "confetti_explosiveness": ConfettiExplosiveness.range_end,
-        "player_sprite": PlayerSprite.option_duck,
-    },
-}
